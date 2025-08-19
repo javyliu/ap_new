3ec85c3dd14be4d5ba757cb35896eba7fc6732c0
qmliu <qmliu@pearlinpalm.com>
2025-08-19 15:16:26 +0800

    rails g authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,30 @@
 class WelcomeController < ApplicationController
-  def index
-    puts "hello"
-  end
+  # USERS = { "admin" => "helloworld" }
+
+  # before_action :authenticate
+
+  # def index
+  #   puts "hello"
+  # end
+
+
+  # private
+  #   def authenticate
+  #     puts params.inspect
+  #     authenticate_or_request_with_http_digest do |username|
+  #       USERS[username]
+  #     end
+  #   end
+
+
+  # TOKEN = "111secret111"
+
+  # before_action :authenticate
+
+  # private
+  #   def authenticate
+  #     authenticate_or_request_with_http_token do |token, options|
+  #       ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+  #     end
+  #   end
 end
